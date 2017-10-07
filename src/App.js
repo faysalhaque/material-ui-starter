648d72b741d76968982cc47f2f10ad6f1a754ac7
halumz <mashuksadman@gmail.com>
2017-10-07 17:08:43 +0600

    1st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Badges from './container/badges';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Badges />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/container/badges.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Badge from 'material-ui/Badge';
+import MailIcon from 'material-ui-icons/Mail';
+import FolderIcon from 'material-ui-icons/Folder';
+import withStyles from 'material-ui/styles/withStyles';
+import withRoot from '../components/withRoot';
+
+const styles = theme => ({
+  badge: {
+    margin: `0 ${theme.spacing.unit * 2}px`
+  }
+});
+
+class Index extends Component {
+  state = {
+    open: false
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div style={{ margin: '50px' }}>
+        <Badge className={classes.badge} badgeContent={4} color="primary">
+          <MailIcon />
+        </Badge>
+        <Badge className={classes.badge} badgeContent={10} color="accent">
+          <FolderIcon />
+        </Badge>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRoot(withStyles(styles)(Index));
